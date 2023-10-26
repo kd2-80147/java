0cce65fcfae1a18097412d7b85f8dcb53cb2dbe9
waikar13 <waikarshub13@gmail.com>
2023-10-27 00:06:26 +0530

    Assignment_12_Ques4/  complited

---
+++ Assignment_12_Ques4/src/com/sunbeam/Check.java
@@ -0,0 +1,7 @@
+package com.sunbeam;
+
+@FunctionalInterface
+public interface Check <T>{
+	boolean compare(T x, T y);
+
+}
---
+++ Assignment_12_Ques4/src/com/sunbeam/MainQues4.java
@@ -0,0 +1,24 @@
+package com.sunbeam;
+
+public class MainQues4 {
+	
+	static <T> int countIf(T[] arr, T key, Check<T> c) {
+		int count = 0;
+		for(T ele : arr) {
+			if(c.compare(ele, key)) {
+				count++;
+			}
+		}		
+		return count;
+	}
+
+	public static void main(String[] args) {
+		Integer [] arr = {44, 77, 99, 22, 55, 66};
+		Integer key = 50;
+		
+		int cnt = countIf(arr, key, (x,y)-> x > y);
+		System.out.println("Count = " + cnt); 
+
+	}
+
+}
