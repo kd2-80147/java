62972e957f060d0c0de7afac4213b818e5fdab7f
waikar13 <waikarshub13@gmail.com>
2023-10-27 00:05:12 +0530

    Assignment_12_Ques2/  complited

---
+++ Assignment_12_Ques2/src/com/sunbeam/Ques2.java
@@ -0,0 +1,21 @@
+package com.sunbeam;
+
+import java.util.function.Predicate;
+
+public class Ques2 {
+	public static int countIf(String[] arr, Predicate<String> cond) {
+		int count = 0;
+		for (String str : arr) {
+			if (cond.test(str))
+				count++;
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		String[] arr = { "Nilesh", "Shubham", "Pratik", "Omkar", "Prashant" };
+		
+		int cnt = countIf(arr, (s) -> s.length() > 6);
+		System.out.println("Result: " + cnt); 
+	}
+}
